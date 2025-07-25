24c29dc1f6065726cb3a2c9910673f6385d94b48
kumargautampushkar <kumargautampushkar@gmail.com>
2025-07-25 22:58:15 +0530

    Backtracking Added

--- Free-Practice/BKTR_01_subset.cpp
+++ Free-Practice/BKTR_01_subset.cpp
@@ -1,15 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void subsetStr(string &s,int n,vector<string> &v){
-    if(n == s.size()) return;
-    string s1 ="";
-    string s2 ="";
-    s1=
+void findSubsets(char * input, char* output, int i, int j)
+{
+    if (input[i] == '\0')
+    {
+        output[j] = '\0';
+        if(output[0] == '\0') {
+            cout << "NULL" << endl;
+            return;
+        }
+        cout << output << endl;
+        return;
+    }
+        
+    output[j] = input[i];
+    // Exclude the current character
+    findSubsets(input, output, i + 1, j+1);
+    // Include the current character
+    findSubsets(input, output, i + 1, j);
+}
+
+void findSubsets2(char * input, char* output, int i, int j)
+{
+    if (input[i] == '\0')
+    {
+        output[j] = '\0';
+        if(output[0] == '\0') {
+            cout << "NULL" << endl;
+            return;
+        }
+        cout << output << endl;
+        return;
+    }
+        
+    
+    // Include the current character
+    findSubsets2(input, output, i + 1, j);
+    output[j] = input[i];
+    // Exclude the current character
+    findSubsets2(input, output, i + 1, j+1);
 }
 
 int main(int argc, char const *argv[])
 {
-    
+    char input[1000],output[1000];
+    cin >> input;
+    findSubsets(input, output, 0, 0);
+    findSubsets2(input, output, 0, 0);
     return 0;
 }
